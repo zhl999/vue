44de5caf01570afe3ffa57dfaafdaa6501787729
zhl <zhl_66@163.com>
2019-08-06 11:04:42 +0800

    vue

---
+++ src/components/Test.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <span>123324567890-</span>
+  <div id="app-6">
+    <input v-model="num1"> +
+    <input v-model="num2"> =
+    {{ parseInt(num1) + parseInt(num2) }}
+  </div>
+</div>
+
+</template>
+<script>
+export default {
+  data () {
+    return {
+      num1: '2',
+      num2: '2'
+    }
+  }
+    // methods: {
+    //     reversedMessage: function () {
+    //         return this.message.split('').reverse().join('')
+    //     }
+    // }
+}
+</script>
+<style scoped>
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Test from '@/components/Test'
+import Index from '@/components/Index'
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/Test',
+      name: 'Test',
+      component: Test
+    },
+    {
+      path: '/Index',
+      name: 'Index',
+      component: Index,
+    }
+  ]
+})
